82bf1b918af24cdb982d3e8c153aeeda25e1ceb1
Sumeet Singh <ortuna@gmail.com>
2013-07-18 18:54:56 -0400

    Added flatui

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-
+Writing::Application.configure do
+  config.assets.paths << "#{Rails.root}/app/assets/fonts"
+end
 # Initialize the Rails application.
 Writing::Application.initialize!
