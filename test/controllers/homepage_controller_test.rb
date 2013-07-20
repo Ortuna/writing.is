0b9ad7c2c2c28241186fd4427f2c3692f14b6ad2
Sumeet Singh <ortuna@gmail.com>
2013-07-19 21:41:25 -0400

    Test fix

--- test/controllers/homepage_controller_test.rb
+++ test/controllers/homepage_controller_test.rb
@@ -1,5 +1,5 @@
 require 'test_helper'
-class ApplicationControllerTest < ActionController::TestCase
+class HomepageControllerTest < ActionController::TestCase
   
   test "should get index" do
     get :index
