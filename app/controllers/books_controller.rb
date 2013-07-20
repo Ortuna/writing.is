bd5ce1ccdcdd9373d39e6a801bdc2a43d103172b
Sumeet Singh <ortuna@gmail.com>
2013-07-19 23:26:38 -0400

    Book lists

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,16 @@
 class BooksController < BaseController
   protect_from_forgery with: :exception
+
   def show
     render text: 'done'
   end
 
   def index
     
+  end
+
+  private 
+  def current_user
+    @current_user ||= current_user
   end
 end
