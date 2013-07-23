6c28428e669619e626b4ed4d1f17a3871a77342e
Sumeet Singh <ortuna@gmail.com>
2013-07-23 15:46:20 -0400

    Specs for repo lists

--- spec/controllers/repos_controller_spec.rb
+++ spec/controllers/repos_controller_spec.rb
@@ -10,16 +10,35 @@ describe ReposController do
     }
   end
 
-  before(:each) do
-    @user = User.create(user_hash)
-    session[:user_id] = @user[:id]
+  def repo_hash
+    [{"name" => 'example1', "description" => 'desc1'},
+     {"name" => 'example2', "description" => 'desc2'}]
   end
 
-  it 'can list all the repos' do
-    require 'pry';binding.pry
-    controller.should_receive(:get_user_repos).and_return('a')
-    get :index
-    response.should == 'a'
+  before(:each) do
+    @user = User.create(user_hash)
+    controller.stub(:current_user).and_return(@user)
   end
+
+  describe :index do 
+    render_views
+    it 'it should render the correct template' do
+      controller.stub(:get_user_repos).and_return(repo_hash)
+      get :index
+      expect(response).to render_template("index")
+    end
+
+    it 'can list all the repos' do
+      
+      controller.stub(:get_user_repos).and_return(repo_hash)
+      get :index
+      expect(response.body).to match /example1/m
+      expect(response.body).to match /example2/m
+
+      expect(response.body).to match /desc1/m
+      expect(response.body).to match /desc2/m
+    end
+  end
+
 
 end
